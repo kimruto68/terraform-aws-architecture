ad5cad804a523cdc0d38f2abf027d450e2677c9f
kimruto68 <kimruto68@gmail.com>
2026-04-28 23:16:04 +0300

    Additional of the portfolio website and changes to terraform file

---
+++ modules/compute/docker_host.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # tighten later
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "dockerhost" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "docker_key"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    dnf update -y
+    dnf install -y docker
+
+    systemctl start docker
+    systemctl enable docker
+
+    usermod -aG docker ec2-user
+
+    docker run -d -p 80:80 --name nginxserver nginx
+  EOF
+
+  tags = {
+    Name = "Terraform-Docker-Host"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.dockerhost.public_ip
+}
